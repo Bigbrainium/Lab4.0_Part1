c913418af31786678dd0b1d963e7caf626420321
Bigbrainium <prashunregmi15@gmail.com>
2023-09-15 09:44:17 -0400

    epic lab 4 part 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+        // ints
+        int intOperandA = 12;
+        int intOperandB = 12;
+        int intSum = intOperandA + intOperandB;
+        int intDifference = intOperandA + intOperandB;
+        int intProduct = intOperandA * intOperandB;
+        int intQuotient = intOperandA / intOperandB;
+        int intModulo = intOperandA % intOperandB;
+
+        // doubles
+        double doubleOperandA = 24;
+        double doubleOperandB = 24;
+        double doubleSum = doubleOperandA + doubleOperandB;
+        double doubleProduct = doubleOperandA * doubleOperandB;
+        double doubleDifference = doubleOperandA - doubleOperandB;
+        double doubleQuotient = doubleOperandA / doubleOperandB;
+        double doubleModul = doubleOperandA % doubleOperandB;
+
+        System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is: " + intSum);
+        System.out.println("The difference of " + intOperandA + " and " + intOperandB + " is: " + intDifference);
+        System.out.println("The product of " + intOperandA + " and " + intOperandB + " is: " + intProduct);
+        System.out.println("The quotient of " + intOperandA + " and " + intOperandB + " is: " + intQuotient);
+        System.out.println("The module of " + intOperandA + " and " + intOperandB + " is: " + intModulo);
+
+        System.out.println("The sum of " + doubleOperandA + " and " + doubleOperandB + " is: " + doubleSum);
+        System.out.println("The difference of " + doubleOperandA + " and " + doubleOperandB + " is: " + doubleDifference);
+        System.out.println("The product of " + doubleOperandA + " and " + doubleOperandB + " is: " + doubleProduct);
+        System.out.println("The quotient of " + doubleOperandA + " and " + doubleOperandB + " is: " + doubleQuotient);
+        System.out.println("The module of " + doubleOperandA + " and " + doubleOperandB + " is: " + doubleModul);
+
     }
 }
